54a9767c21c58946d229f7575e6aebce8c2b4fc7
Rafa <rafumojica@gmail.com>
2026-03-01 17:43:05 -0800

    [step 8 ] complete??

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import wordSets from "../data/wordSets";
 
 const GameScreen = ({ navigation }) => {
   const [selectedMemberId, setSelectedMemberId] = useState();
-  const [wordSet, setWordSet] = useState([]);
+  const [wordSet, setWordSet] = useState({});
   const [imposterIndex, setImposterIndex] = useState();
 
   // randomly select indexes
-  // Math.random(Math.floor(index))
+  // Math.floor(Math.random() * range);
+
+  const wordSetLength = wordSets.length();
 
   useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * wordSetLength); // get random index on mount
+    setWordSet(wordSets[randomIndex]); // get word set and store it
+    setSelectedMemberId(0); // reset selected member id
+  })
 
-  }, [])
-  
   return (
     <View>
       <Text>Game Screen</Text>
